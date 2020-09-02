f282568fba74c34181ca8a1ee7c5895b1777e407
vdo <vdo@greyfaze.net>
2020-09-02 12:39:36 +0200

    Add script and README

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Vocstack
 
 Vocdoni stack deployment on docker and k8s
+
+# Local compose
+
+To deploy a local development environment, first you need to add the following to `/etc/hosts`:
+
+```
+127.0.0.1 vocstack.local
+```
+
+Once ready, you can run `run_local_vocstack.sh` to checkout, build and deploy all components from master branch.
+
+If you want to use a diferent branch for a specific component, you can do like following (before running any script):
+
+```
+git submodule set-branch --branch release-0.5 -- external/go-dvote
+```
+
+## k8s
+
+To-Do
---
+++ run_local_vocstack.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+if ! ping -c 1 vocstack.local >/dev/null; then
+    echo -e "${RED}vocstack.local does not exist. Add \"127.0.0.1 vocstack.local\" to your /etc/hosts${NOCOLOR}"
+    exit 1
+fi
+
+echo -e "${GREEN}Updating submodules...${NOCOLOR}"
+git submodule update --recursive --remote
+
+pushd docker/vocstack_local
+echo -e "${GREEN}Building components...${NOCOLOR}"
+docker-compose build
+
+echo -e "${GREEN}Building components...${NOCOLOR}"
+docker-compose up -d
+
+echo -e "${GREEN}Ready! \nFrontend running at http://vocstack.local:8081\nVocexplorer running at http://vocstack.local:8082${NOCOLOR}"
